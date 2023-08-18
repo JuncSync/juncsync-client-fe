3cd3f01332bcad7bad95a4b0ed8beb84ec632c15
stpcoder <thbrian@postech.ac.kr>
2023-08-19 03:00:08 +0900

    chore: getGeo

--- src/pageComponents/HomePageMain.tsx
+++ src/pageComponents/HomePageMain.tsx
@@ -1,5 +1,16 @@
+import useGeolocation from '@/hooks/query/common/useGeolocation';
+
 const HomePageMain = () => {
-  return <div>메인 홈페이지</div>;
+  const location = useGeolocation();
+
+  return (
+    <div>
+      메인 홈페이지
+      {location.loaded
+        ? JSON.stringify(location)
+        : 'Location data not available yet.'}
+    </div>
+  );
 };
 
 export default HomePageMain;
