77a4868bd8521bb55c9ce32229e038583029bd7d
stpcoder <thbrian@postech.ac.kr>
2023-08-20 09:28:28 +0900

    feat: curr position dot

--- src/component/Mapuse/index.tsx
+++ src/component/Mapuse/index.tsx
@@ -181,9 +181,16 @@ function Mapcom() {
           ))}
           <Polyline
             path={dot}
-            strokeWeight={5} // 선의 두께 입니다
-            strokeColor={'#FFAE00'} // 선의 색깔입니다
-            strokeOpacity={0.7} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+            strokeWeight={17} // 선의 두께 입니다
+            strokeColor={'#ffffff'} // 선의 색깔입니다
+            strokeOpacity={1} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+            strokeStyle={'solid'} // 선의 스타일입니다
+          />
+          <Polyline
+            path={dot}
+            strokeWeight={12} // 선의 두께 입니다
+            strokeColor={'#ff0000'} // 선의 색깔입니다
+            strokeOpacity={1} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
             strokeStyle={'solid'} // 선의 스타일입니다
           />
         </Map>
